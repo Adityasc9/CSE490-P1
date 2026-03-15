ee8e978a465530f5bc225fa1e0c160cbba03b005
Shouvik Das <shouvik350@gmail.com>
2026-03-14 21:30:13 -0400

    Added SRC folder and PC file

---
+++ Src/PC.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 06:10:33 PM
+// Design Name: 
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC(
+    input wire[15:0] NextAddr,
+    output wire[15:0] CurrAddr
+    );
+    
+    reg[15:0] address;
+    reg[15:0] curr;
+    
+    initial
+    begin
+        address = 16'h0000;
+    end
+    
+    always@ (NextAddr)
+    begin
+        curr = address;
+        address = NextAddr;
+    end
+    
+    assign CurrAddr = curr;
+
+endmodule
