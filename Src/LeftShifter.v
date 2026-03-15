0629b5256644e057a2c63d294cba61f4ad4e53c7
Adityasc9 <116690197+Adityasc9@users.noreply.github.com>
2026-03-14 21:50:40 -0400

    Added Left Shifter and Mux 2 to 1

---
+++ Src/LeftShifter.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 10:21:48 PM
+// Design Name: 
+// Module Name: LeftShifter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module LeftShifter(
+    input [15:0] Data_In,
+    output [15:0] Data_Out
+    );
+    
+    reg [15:0] tmp = 16'h0000;
+    always@ (Data_In)
+    begin
+        tmp[15:1] = Data_In[14:0];
+    end
+    
+    assign Data_Out = tmp;
+    
+endmodule
---
+++ Src/MUX2to1.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 06:10:33 PM
+// Design Name: 
+// Module Name: MUX2to1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX2to1(
+    input wire[15:0] A,
+    input wire[15:0] B,
+    input wire Select,
+    output wire[15:0] Out
+    );
+    
+    reg[15:0] data_out;
+    
+    always@(A,B,Select)
+    begin
+        case(Select)
+            1'b0: data_out = A;
+            1'b1: data_out = B;
+        endcase
+    end
+    
+    assign Out = data_out;
+endmodule
