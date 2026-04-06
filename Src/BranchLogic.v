7c3f5df078fadf8089422d58dab89b0aec0892d3
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2026-04-06 16:29:51 -0400

    Add BranchLogic module
    Implement BranchLogic module for conditional branching (BNE/BEQ)

---
+++ Src/BranchLogic.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 10:29:47 PM
+// Design Name: 
+// Module Name: BranchLogic
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BranchLogic(
+    input wire[1:0] BranchCntrl,
+    input wire[1:0] ALUFlags,
+    output wire Result
+    );
+    
+    reg r;
+    
+    always@ (BranchCntrl, ALUFlags)
+    begin
+        if ((BranchCntrl == 2'b01) && (ALUFlags == 2'b01))
+            r = 1'b1;
+        else if ((BranchCntrl == 2'b10) && (ALUFlags == 2'b10))
+            r = 1'b1;
+        else 
+            r = 1'b0;
+    end
+    
+    assign Result = r;
+    
+endmodule
