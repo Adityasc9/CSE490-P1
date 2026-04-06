bc66fc202264b943e350811cd91af51e7ca54a3e
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2026-04-06 16:30:25 -0400

    Add JumpLogic module for address calculation

---
+++ Src/JumpLogic.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 11:17:31 PM
+// Design Name: 
+// Module Name: JumpLogic
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module JumpLogic(
+    input [15:0] PC,
+    input [15:0] Offset,
+    output [15:0] Result
+    );
+    
+    reg [15:0] jmp_addr;
+    
+    always@ (PC, Offset)
+    begin
+        jmp_addr = PC + Offset;
+    end
+    
+    assign Result = jmp_addr;
+
+endmodule
