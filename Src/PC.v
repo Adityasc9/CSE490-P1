3767756153b656d3804bc44d059a501fce7f549e
Adityasc9 <116690197+Adityasc9@users.noreply.github.com>
2026-04-07 00:25:49 -0400

    Update PC to be one cycle

--- Src/PC.v
+++ Src/PC.v
@@ -26,64 +26,18 @@ module PC(
     output wire[15:0] CurrAddr
     );
     
-    reg[15:0] address;
-    reg[15:0] curr;
+    reg[15:0] pc_reg;
     initial
 
     begin
-        address = 16'h0000;
+        pc_reg = 16'h0000;
     end
     
     always@ (posedge Clock)
     begin
-        curr = address;
-        address = NextAddr;
+        pc_reg <= NextAddr;
     end
     
-    assign CurrAddr = curr;
-
-endmodule
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2026 06:10:33 PM
-// Design Name: 
-// Module Name: PC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module PC(
-    input wire[15:0] NextAddr,
-    output wire[15:0] CurrAddr
-    );
-    
-    reg[15:0] address;
-    reg[15:0] curr;
-    
-    initial
-    begin
-        address = 16'h0000;
-    end
-    
-    always@ (NextAddr)
-    begin
-        curr = address;
-        address = NextAddr;
-    end
-    
-    assign CurrAddr = curr;
+    assign CurrAddr = pc_reg;
 
 endmodule
