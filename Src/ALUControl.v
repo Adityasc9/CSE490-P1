0e3577aba7c14997f6a3e02616979deef9a394c0
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2026-04-07 21:05:30 -0400

    Create ALUControl module for ALU operation selection

---
+++ Src/ALUControl.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2026 09:01:05 PM
+// Design Name: 
+// Module Name: ALUControl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALUControl(
+    input wire [1:0] ALUOp,
+    input wire [3:0] Funct,
+    output wire [2:0] Operation
+    );
+    
+    reg [2:0] op;
+    
+    always@ (ALUOp, Funct)
+    begin
+        case(ALUOp)
+            2'b00: op = 3'b000;     // lw, sw, addi need ALU to add
+            2'b01: op = 3'b001;     // beq, bne need ALU to subtract
+            2'b10: op = Funct[2:0]; // R-type can use funct field
+            default: op = 3'b000;   // Don't care
+        endcase
+    end
+    
+    assign Operation = op;
+  
+endmodule
